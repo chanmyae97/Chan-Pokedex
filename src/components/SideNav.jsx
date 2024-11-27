bb8a40ca5a4c9f756800c6a1d26cc36e5581f8ea
Aung Chan Myae <aungchanmyae10997@gmail.com>
2024-11-27 15:48:31 +0700

    Start a project of pokedex

---
+++ src/components/SideNav.jsx
@@ -0,0 +1,19 @@
+import { first151Pokemon, getFullPokedexNumber } from "../utils";
+
+export function SideNav() {
+  return (
+    <nav>
+      <div className="header"></div>
+      <h1 className="text-gradient">Pokédex</h1>
+      <input placeholder="Eg. 001 or Bulba..." />
+      {first151Pokemon.map((pokemon, pokemonIndex) => {
+        return (
+          <button className={"nav-card "}>
+            <p>{getFullPokedexNumber(pokemonIndex)}</p>
+            <p>{pokemon}</p>
+          </button>
+        );
+      })}
+    </nav>
+  );
+}
